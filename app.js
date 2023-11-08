02c560589f93ec3e8ad7067fc9e334773b2990af
Dmitry <89023913+dv-opel@users.noreply.github.com>
2023-11-08 15:15:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+tg.MainButton.setText("Натисни!");
 tg.MainButton.show();
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
