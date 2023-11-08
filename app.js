813759e6cd1a0dfa9560727e0e014edffe427f7b
Dmitry <89023913+dv-opel@users.noreply.github.com>
2023-11-08 15:12:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
+tg.MainButton.show();
+
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData("Hello from web");
 });
