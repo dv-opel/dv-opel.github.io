64dcfb6307dcd76bd07ee4d39b57c96376457eb0
Dmitry <89023913+dv-opel@users.noreply.github.com>
2023-11-20 14:33:22 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ tg.MainButton.color = '#2cab37';
 tg.MainButton.setText("Натисни!");
 tg.MainButton.show();
 
+const firstValue = document.form-select.options[0];
+console.log("Index:", firstValue.index);
+console.log("Text:", firstValue.text);
+console.log("Value:", firstValue.value);
+
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
-	tg.sendData("Hello from web");
+	tg.sendData(firstValue.text);
 });
 
